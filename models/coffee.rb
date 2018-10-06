cf12dcfea5867074e8b44c67e98beefbea09a57b
Genna Walsh <gennawalsh@gmail.com>
2018-10-06 14:00:25 +0100

    add coffee class and save method

---
+++ models/coffee.rb
@@ -0,0 +1,51 @@
+class Coffee
+
+  attr_accessor :id, :name, :roaster_id, :profile, :region, :primary_taste, :in_stock, :total_sold
+
+  def initialize(options)
+    @id = options["id"].to_i if options["id"]
+    @name = options["name"]
+    @roaster_id = options["roaster_id"].to_i if options["roaster_id"]
+    @profile = options["profile"]
+    @region = options["region"]
+    @primary_taste = options["primary_taste"]
+    @in_stock = options["in_stock"]
+    @total_sold = 0
+  end
+
+# .in_stock? method to get boolean.
+      # will be distinct from attribute?
+  def in_stock?()
+    if @in_stock == "true"
+      return true
+    end
+    return false
+  end
+
+# .save
+  def save()
+    sql = "
+      INSERT INTO coffees (
+        name,
+        roaster_id,
+        profile,
+        region,
+        primary_taste,
+        in_stock
+      )
+      VALUES ($1, $2, $3, $4, $5, $6)
+      RETURNING id;
+    "
+    values = [@name, @roaster_id, @profile, @region, @primary_taste, @in_stock]
+    results = SqlRunner.run(sql, values)
+    @id = results[0]["id"].to_i
+  end
+
+# update?
+    # needed?
+
+# self.all
+
+# self.delete_all
+
+end
