2bf3d7c1b67299531ba04c3137e6ff951620ac59
Genna Walsh <gennawalsh@gmail.com>
2018-10-06 13:31:07 +0100

    create db tables: coffee & roasters

---
+++ db/coffee.sql
@@ -0,0 +1,23 @@
+DROP TABLE IF EXISTS roasters;
+DROP TABLE IF EXISTS coffees;
+
+# roasters
+CREATE TABLE roasters (
+  id            SERIAL8 PRIMARY KEY,
+  name          VARCHAR(255),
+  location      VARCHAR(255),
+  style         VARCHAR(255),
+  total_sold     VARCHAR(255)
+);
+
+
+CREATE TABLE coffees (
+  id            SERIAL8 PRIMARY KEY,
+  name          VARCHAR(255),
+  roaster_id    INT8 REFERENCES roasters(id) ON DELETE CASCADE,
+  profile       VARCHAR(255),
+  region        VARCHAR(255),
+  primary_taste VARCHAR(255),
+  in_stock      BOOLEAN,
+  total_sold    INT8
+);
